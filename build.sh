2f8c5f33132ed241a97f27aa2d46441777833407
kojix2 <2xijok@gmail.com>
2024-10-25 13:30:33 +0900

    Change samtools submodule URL and update build script for mruby and samtools

--- build.sh
+++ build.sh
@@ -1,4 +1,22 @@
 #!/usr/bin/env bash
 
+# mruby
 current_dir=$(dirname "${BASH_SOURCE[0]}")
 rake MRUBY_CONFIG=$current_dir/samtools_mruby_build_config.rb -f $current_dir/mruby/Rakefile
+
+# htslib
+cd htslib
+git submodule update --init --recursive
+autoreconf -i
+./configure
+make
+cd ..
+
+# samtools
+cd samtools
+git submodule update --init --recursive
+autoreconf -i
+./configure
+make
+cd ..
+
